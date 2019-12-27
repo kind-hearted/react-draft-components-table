8c7d9c3a9a353026ee62a1d6920f0f41a50722f2
Zhenghua Yu <zhenghua.yu@cignacmb.com>
2019-12-27 18:59:22 +0800

    add

---
+++ src/utils/isNotEdgeIE.js
@@ -0,0 +1,14 @@
+// Edge window.navigator.userAgent
+// Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299
+
+// IE11 window.navigator.userAgent
+// Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; rv:11.0) like Gecko
+
+// IE10 window.navigator.userAgent
+// Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E)
+
+// IE9 window.navigator.userAgent
+// Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E)
+
+// IE8 window.navigator.userAgent
+// Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E)
